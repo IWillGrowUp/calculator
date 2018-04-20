88ea83fecb81937bc01f9116424dd34d30ae9826
Mike <mikhail.romanishin@gmail.com>
2018-04-20 18:28:04 +0000

    almost complete

--- js/index.js
+++ js/index.js
@@ -1,89 +1,58 @@
-document.getElementById('seven').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '7';
-e.preventDefault();
-}));
+function makeCalc (num, writeNum) {
+    document.getElementById(num).addEventListener('click', (function(e) {
+        document.getElementById('display').value += writeNum;
+        e.preventDefault();
+    }));
+}
 
-document.getElementById('eight').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '8';
-e.preventDefault();
-}));
-
-document.getElementById('nine').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '9';
-e.preventDefault();
-}));
+makeCalc('doubleZero', '00');
+makeCalc('zero', '0');
+makeCalc('one', '1');
+makeCalc('two', '2');
+makeCalc('three', '3');
+makeCalc('four', '4');
+makeCalc('five', '5');
+makeCalc('six', '6');
+makeCalc('seven', '7');
+makeCalc('eight', '8');
+makeCalc('nine', '9');
+makeCalc('plus', '+');
+makeCalc('minus', '-');
+makeCalc('divide', '/');
+makeCalc('multiply', '*');
+makeCalc('dot', '.');
 
 document.getElementById('reset').addEventListener('click', (function(e) {
-    document.getElementById('display').value = '';
-e.preventDefault();
-}));
-
-document.getElementById('divide').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '/';
-e.preventDefault();
-}));
-
-document.getElementById('four').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '4';
-e.preventDefault();
-}));
-
-document.getElementById('five').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '5';
-e.preventDefault();
-}));
-
-document.getElementById('six').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '6';
-e.preventDefault();
-}));
-
-document.getElementById('plus').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '+';
-e.preventDefault();
-}));
-
-document.getElementById('minus').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '-';
-e.preventDefault();
-}));
-
-document.getElementById('one').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '1';
-e.preventDefault();
-}));
-
-document.getElementById('two').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '2';
-e.preventDefault();
-}));
-
-document.getElementById('three').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '3';
-e.preventDefault();
-}));
-
-document.getElementById('zero').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '0';
-e.preventDefault();
-}));
-
-document.getElementById('multiply').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '*';
-e.preventDefault();
-}));
-
-document.getElementById('dot').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '.';
-e.preventDefault();
-}));
-
-document.getElementById('doubleZero').addEventListener('click', (function(e) {
-    document.getElementById('display').value += '00';
-e.preventDefault();
-}));
+        document.getElementById('display').value = '';
+        e.preventDefault();
+    }));
 
 document.getElementById('equals').addEventListener('click', (function(e) {
     document.getElementById('display').value = eval(document.getElementById('display').value);
 e.preventDefault();
 }));
+
+function makeSound (forButton, forSound) {
+    document.getElementById(forButton).addEventListener('click', (function(e) {
+            document.getElementById(forSound).play();
+    }));
+}
+
+makeSound('doubleZero', 'button_sound');
+makeSound('zero', 'button_sound');
+makeSound('one', 'button_sound');
+makeSound('two', 'button_sound');
+makeSound('three', 'button_sound');
+makeSound('four', 'button_sound');
+makeSound('five', 'button_sound');
+makeSound('six', 'button_sound');
+makeSound('seven', 'button_sound');
+makeSound('eight', 'button_sound');
+makeSound('nine', 'button_sound');
+makeSound('plus', 'action_sound');
+makeSound('minus', 'action_sound');
+makeSound('divide', 'action_sound');
+makeSound('multiply', 'action_sound');
+makeSound('dot', 'dot_sound');
+makeSound('reset', 'reset_sound');
+makeSound('equals', 'equal_sound');
